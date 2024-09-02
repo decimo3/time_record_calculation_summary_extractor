41376cad5ed32d841d5307e751cd622c8185159b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-02 10:02:01 -0300

    Added file conversion via argument

--- src/index.py
+++ src/index.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 # coding: utf8
 #region imports
+import os
+import sys
 import re
 import pandas
 from pypdf import PdfReader
 from Scheme import Registry
 #endregion imports
 if __name__ == "__main__":
-  with open('src\Resumo de Apurações de Ponto Mesquita.PDF', 'rb') as file:
+  if(len(sys.argv) != 2):
+    raise ValueError()
+  if not (os.path.exists(sys.argv[1])):
+    raise FileNotFoundError()
+  with open(sys.argv[1], 'rb') as file:
     reader = PdfReader(file)
     estabelecimento_texto = ''
     departamento_texto = ''
@@ -18,20 +24,20 @@ if __name__ == "__main__":
       for line_number in range(len(lines)):
         line_text = lines[line_number]
         line_words = line_text.split(' ')
-        # TODO - Coletar 'estabelecimento' e 'departamento'
+        # DONE - Coletar 'estabelecimento' e 'departamento'
         if(line_words[0] == 'Estabelecimento'):
           departamento_index = line_words.index('Departamento')
           estabelecimento_texto = str.join(' ', line_words[2 : departamento_index])
           departamento_texto = str.join(' ', line_words[departamento_index + 2 : len(line_words)])
           continue
-        # TODO - Coletar 'matricula' e 'nome_colaborador'
+        # DONE - Coletar 'matricula' e 'nome_colaborador'
         match = re.findall('^[0-9]{6}', line_words[0])
         if(len(match) == 1):
           Registry['matricula'].append(match[0])
           nome_colaborador = str.join(' ', line_words[2:])
           Registry['nome_colaborador'].append(nome_colaborador)
           continue
-        # TODO - Coletar a lista de horarios
+        # DONE - Coletar a lista de horarios
         match = re.findall('-?[0-9]{2}:[0-9]{2}', line_text)
         if(len(match) != 13): continue
         Registry['saldo_anterior'].append(match[0])
@@ -47,12 +53,12 @@ if __name__ == "__main__":
         Registry['horas_positivas_saldo'].append(match[10])
         Registry['saldo_periodo'].append(match[11])
         Registry['saldo_final'].append(match[12])
-        # TODO - Coletar 'periodo_inicio' e 'final_periodo'
+        # DONE - Coletar 'periodo_inicio' e 'final_periodo'
         match = re.findall('[0-9]{2}/[0-9]{2}', line_text)
         if(len(match) == 2):
           Registry['inicio_periodo'].append(match[0])
           Registry['final_periodo'].append(match[1])
-        # TODO - Define 'estabelecimento' e 'departamento' 
+        # DONE - Define 'estabelecimento' e 'departamento' 
         Registry['estabelecimento'].append(estabelecimento_texto)
         Registry['departamento'].append(departamento_texto)
     dataframe = pandas.DataFrame(Registry)
